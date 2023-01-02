8e764da6f3ba6ce5bea95222b1b8be7e0d8f6623
dylanlauzy <dylzylau@gmail.com>
2023-01-02 01:49:31 -0500

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false })) // to allow for receiving of ur
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-// Serving the frontend
+// Serve the frontend
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')))
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
