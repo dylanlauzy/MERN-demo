a9a799e6b5e0373abd064ca719e777711683011b
dylanlauzy <dylzylau@gmail.com>
2023-01-01 19:19:56 -0500

    Connected frontend with backend for User API. Goals API not working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,14 +37,13 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
     throw new Error('Invalid user data')
   }
-
-  res.json({message: 'Register User'})
 })
 
 // @desc Authenticate a user
@@ -73,12 +72,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user.id)
-  res.status(200).json({
-    id: _id,
-    name,
-    email
-  })
+  res.status(200).json(req.user)
 })
 
 // Generate JWT
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,43 @@
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
+import {toast} from 'react-toastify'
+
 function Dashboard() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
+
+  useEffect(() => {
+    if(!user) {
+      navigate('/login')
+    }
+    
+    if(isError) {
+      toast.error(message)
+    }
+    
+    dispatch(getGoals())
+  }, [user, navigate, isError, message])
+
+  if(isLoading) {
+    return <Spinner />
+  }
+
   return (
-    <div>dashboard</div>
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+    </>
   )
 }
 
